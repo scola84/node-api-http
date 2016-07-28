4951627df7c8d1958e48ef9fa7b16192877b4e15
M.C. van den Boogaart <scola@kvdb.name>
2016-07-28 11:53:26 +0200

    Only export class

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
+export { default as Connector} from './src/connector';
 export { default as ServerRequest } from './src/server-request';
 export { default as ServerResponse } from './src/server-response';
-
-import Connector from './src/connector';
-export { Connector };
-
-export function connector(server, router) {
-  return new Connector(server, router);
-}
