a264d777662bb60361dba3c17c0fb124ca7d3921
M.C. van den Boogaart <scola@kvdb.name>
2017-03-03 21:26:09 +0100

    Refactor exports

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
+import HttpConnection from './src/connection';
+import HttpConnector from './src/connector';
+import ServerRequest from './src/server-request';
+import ServerResponse from './src/server-response';
+import Writer from './src/helper/writer';
+import parseHeader from './src/helper/parse-header';
 import strings from './src/i18n/strings';
 
-export { default as HttpConnection } from './src/connection';
-export { default as HttpConnector } from './src/connector';
-export { default as ServerRequest } from './src/server-request';
-export { default as ServerResponse } from './src/server-response';
-export { default as Writer } from './src/helper/writer';
-export { default as parseHeader } from './src/helper/parse-header';
-
-export function load(app) {
+function load(app) {
   if (app.i18n()) {
     app.i18n().strings(strings);
   }
 }
+
+export {
+  HttpConnection,
+  HttpConnector,
+  ServerRequest,
+  ServerResponse,
+  Writer,
+  parseHeader,
+  load
+};
