ceeda163b2c102efe9e4e33b1ef6b5eff7fde66f
M.C. van den Boogaart <scola@kvdb.name>
2017-03-18 17:22:39 +0100

    Update error messages

--- src/i18n/en.js
+++ src/i18n/en.js
@@ -1,11 +1,17 @@
 export default {
   scola: {
     error: {
+      invalid_data: 'The data was not found.',
+      invalid_query: 'A server error occurred.',
       invalid_request: 'A message error occurred.',
       invalid_response: 'A message error occurred.',
+      invalid_state: 'Your request is being processed.',
       short: {
+        invalid_data: 'No Data',
+        invalid_query: 'Server Error',
         invalid_request: 'Message Error',
-        invalid_response: 'Message Error'
+        invalid_response: 'Message Error',
+        invalid_state: 'Processing Error'
       }
     }
   }
--- src/i18n/nl.js
+++ src/i18n/nl.js
@@ -1,11 +1,17 @@
 export default {
   scola: {
     error: {
+      invalid_data: 'De gegevens zijn niet gevonden.',
+      invalid_query: 'Er is een serverfout opgetreden.',
       invalid_request: 'Er is een berichtfout opgetreden.',
       invalid_response: 'Er is een berichtfout opgetreden.',
+      invalid_state: 'Uw aanvraag wordt verwerkt.',
       short: {
+        invalid_data: 'Geen gegevens',
+        invalid_query: 'Serverfout',
         invalid_request: 'Berichtfout',
-        invalid_response: 'Berichtfout'
+        invalid_response: 'Berichtfout',
+        invalid_state: 'Verwerkingsfout'
       }
     }
   }
